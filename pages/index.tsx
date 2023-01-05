ae0b118bf5c9a7d0003e56e8c43c2898032717d8
kev-ng <kwng.kevin@gmail.com>
2023-01-05 23:05:52 +0800

    connect to supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,60 @@
 import Image from 'next/image'
 import {useState} from 'react'
+import {createClient} from '@supabase/supabase-js'
 
-export default function Gallery(){
+
+export async function getStaticProps(){
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  );
+  
+  const {data} = await supabaseAdmin.from('images').select('*').order('id');
+  return{
+    props:{
+      images:data
+    }
+  };
+}
+function cn(...classes: string[]){
+  return classes.filter(Boolean).join(' ');
+}
+
+type Image = {
+  id: number
+  href: string
+  imageSrc: string
+  name: string
+  username: string
+}
+
+export default function Gallery({images}:{images: Image[]}){
   return(
     <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
       <div className='grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
-        {/* images here */}
+        {images.map((image)=>(
+        <BlurImage key={image.id} image={image}/>
+        ))}
       </div>
     </div>
   )
 }
 
-function cn(...classes: string[]){
-  return classes.filter(Boolean).join(' ');
-}
 
-function BlurImage(){
+
+function BlurImage({image}:{image:Image}){
   const [isLoading, setLoading] = useState(true);
   return(
     
-    <a href='#' className='group'>
+    <a href={image.href} className='group'>
       <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
       <Image
        alt=""
-       src="https://bit.ly/placeholder-img"
-       layout='fill'
-       objectFit='cover'
+       src={image.imageSrc}
+       fill
+       style={{
+        objectFit:'cover'
+       }}
        className={cn(
         'group-hover:opacity-75 duration-700 ease-in-out',
         isLoading
@@ -35,8 +64,8 @@ function BlurImage(){
        onLoadingComplete={()=>setLoading(false)}
        />
       </div>
-    <h3 className='mt-4 text-sm text-gray-700'> Lee Robinson</h3>
-    <p className='mt-1 text-lg font-medium text-gray-900'>@leerob</p>
+    <h3 className='mt-4 text-sm text-gray-700'> {image.name}</h3>
+    <p className='mt-1 text-lg font-medium text-gray-900'>{image.username}</p>
     </a>
   );
 }
